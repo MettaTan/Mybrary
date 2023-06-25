d9d06af3ce069f50c7c8391a1bc753f0934d34df
Metta <mettahytan@gmail.com>
2023-06-25 10:55:08 +0800

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All authors route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', 
+        { authors: authors, 
+        searchOptions: req.query
+    })
+    } catch (err){
+        console.log(err)
+        res.redirect('/')
+    }
+
+})
+
+// New authors route
+router.get('/new', (req,res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+// Create author route
+router.post('/', (req,res) => {
+    async function createAuthor(){
+
+        const author = new Author({
+            name: req.body.name
+        })
+    
+        try {
+            const newAuthor = await author.save()
+            res.redirect('authors')
+        } catch (err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating author'
+            })
+        }
+    }
+    createAuthor()
+})
+
+
+
+module.exports = router
